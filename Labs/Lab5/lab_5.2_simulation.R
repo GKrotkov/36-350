235a149751a8669122f1dd724bc709f043a5bfc0
Gabriel Garvin Krotkov <Gabriel@Gabriels-MBP.wv.cc.cmu.edu>
2018-08-02 12:22:47 -0400

    Renaming file

---
+++ Labs/Lab5/lab_5.2_simulation.R
@@ -0,0 +1,11 @@
+generate_data <- function(n, p){
+  cov <- matrix(rnorm(n * p), nrow = n, ncol = p)
+  resp <- rnorm(n)
+  return(list(covariates = cov, 
+              responses = resp))
+}
+
+model_select(covariates, responses, cutoff){
+  lmodel <- lm(responses ~ covariates)
+  
+}
