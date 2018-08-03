92ea7556f0ec81aa38c35c08ef1f7f64187bc109
Gabriel Garvin Krotkov <Gabriel@Gabriels-MBP.wv.cc.cmu.edu>
2018-08-03 12:20:01 -0400

    Lab 2c completed

--- Labs/Lab5/lab_5.2_simulation.R
+++ Labs/Lab5/lab_5.2_simulation.R
@@ -10,11 +10,22 @@ model_select <- function(covariates, responses, cutoff = 0.05){
   coeffs <- summary(lmodel)$coefficients
   select <- coeffs[,ncol(coeffs)] <= cutoff
   # -1 removes the constant term
-  newCov <- matrix(covariates[, select[-1]])
-  browser()
-  if(ncol(newCov) == 0){
+  newCov <- matrix(covariates[, select[-1]], nrow = length(responses))
+  if(ncol(newCov) == 0 | nrow(newCov) == 0){
     return(vector())
   }
   refit <- lm(responses ~ newCov)
-  return(summary(refit)$coefficients)
+  # -1 removes the constant term again
+  return(summary(refit)$coefficients[,ncol(coeffs)][-1])
+}
+
+run_simulation <- function(n_trials, n, p, cutoff = 1){
+  result = vector()
+  for(i in 1:n_trials){
+    data <- generate_data(n, p)
+    result <- c(result, model_select(data$covariates, 
+                                     data$responses, 
+                                     cutoff = cutoff))
+  }
+  hist(result)
 }
